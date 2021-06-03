ea1f981dbfd10b52b30eaddb03208a5c111985ae
Tiago Oliveira <tiagolivesan@outlook.com>
2021-06-03 15:39:48 -0300

    react next auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,28 @@
-import { useState } from 'react'
+import { FormEvent, useContext, useState } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-	const [email, setEmail] = useState ('');
-	const [password, setPassword] = useState ('');
+	const [email, setEmail] = useState (' ');
+	const [password, setPassword] = useState (' ');
+	
+	const { signIn } = useContext(AuthContext)
 
-	function handleSubmit() {
+	async function handleSubmit(event: FormEvent) {
+		event.preventDefault();
+
 		const data = {
 			email,
 			password,
 		}
 
-		console.log(data);
+		await signIn(data);
 	}
 
   return (
     <form onSubmit={handleSubmit} className={styles.container}>
-      <input type="email" value={email} onChange={ e => setEmail(e.target.value)} />
+      <input type="email" value={email} onChange = { e => setEmail(e.target.value)} />
 			<input type="password" value={password} onChange = { e => setPassword(e.target.value)} />
 			<button type="submit"> Entrar </button>
     </form>
