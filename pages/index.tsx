8cb04a8dd0adf6f4e569d72fd3414353c94c2da2
tosantos1 <tiagolivesan@outlook.com>
2021-07-01 11:26:56 -0300

    Recuperando token no server-side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import { GetServerSideProps } from 'next';
+import { parseCookies } from 'nookies';
+
 import { FormEvent, useContext, useState } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
-
-import { parseCookies } from 'nookies'
 
 import styles from '../styles/Home.module.css'
 
@@ -34,6 +34,14 @@ export default function Home() {
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	const cookies = parseCookies(ctx)
 
+	if (cookies['nextauth.token']) {
+	return {
+		redirect: {
+			destination: '/dashboard',
+			permanent: false,
+		}
+	}
+}
 	return {
 		props: {}
 	}
