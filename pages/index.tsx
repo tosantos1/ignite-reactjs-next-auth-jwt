e934d854c2bfd390e111ab19366b222ee25b89b1
Tiago Oliveira <tiagolivesan@outlook.com>
2021-07-02 21:44:47 -0300

    Validando visitante

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,12 @@ import { FormEvent, useContext, useState } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
 
 import styles from '../styles/Home.module.css'
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 export default function Home() {
-	const [email, setEmail] = useState (' ');
-	const [password, setPassword] = useState (' ');
-	
+	const [email, setEmail] = useState(' ');
+	const [password, setPassword] = useState(' ');
+
 	const { signIn } = useContext(AuthContext)
 
 	async function handleSubmit(event: FormEvent) {
@@ -23,26 +24,17 @@ export default function Home() {
 		await signIn(data);
 	}
 
-  return (
-    <form onSubmit={handleSubmit} className={styles.container}>
-      <input type="email" value={email} onChange = { e => setEmail(e.target.value)} />
-			<input type="password" value={password} onChange = { e => setPassword(e.target.value)} />
+	return (
+		<form onSubmit={handleSubmit} className={styles.container}>
+			<input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+			<input type="password" value={password} onChange={e => setPassword(e.target.value)} />
 			<button type="submit"> Entrar </button>
-    </form>
-  )
+		</form>
+	)
 }
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const cookies = parseCookies(ctx)
 
-	if (cookies['nextauth.token']) {
-	return {
-		redirect: {
-			destination: '/dashboard',
-			permanent: false,
-		}
-	}
-}
+export const getServerSideProps = withSSRGuest(async (ctx) => {
 	return {
 		props: {}
 	}
-}
+})
